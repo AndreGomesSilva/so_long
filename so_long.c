16ffc2b401eb34f8f116f0485206110faa83e05d
Andre Gomes da silva <angomes-@c3r3p3.42sp.org.br>
2023-08-03 19:06:11 -0300

    understand rect

--- so_long.c
+++ so_long.c
@@ -1,35 +1,82 @@
-#include <mlx.h>
 #include "so_long.h"
+#include <mlx.h>
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-void my_mlx_pixel_put(t_data *data, int x, int y, int color)
+typedef struct s_data
 {
-    char *dst;
+	void	*mlx_ptr;
+	void	*win_ptr;
+}			t_data;
 
-    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-    *(unsigned int*)dst = color;
+typedef struct s_rect
+{
+	int		x;
+	int		y;
+	int		width;
+	int		heigth;
+	int		color;
+}			t_rect;
+// void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+// {
+// 	char	*dst;
+//
+// 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+// 	*(unsigned int *)dst = color;
+// }
+
+int	render_rect(t_data *data, t_rect rect)
+{
+	int	i;
+	int	j;
+
+	if (!data->win_ptr)
+		return (MLX_ERROR);
+	i = rect.y;
+	while (i < rect.y + rect.heigth)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
+		++i;
+	}
+	return (0);
+}
+
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->mlx_ptr);
+		data->win_ptr = NULL;
+	}
+	return (0);
+}
+
+int	render(t_data *data)
+{
+	render_rect(data, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100,
+			100, 0xFF00});
+	render_rect(data, (t_rect){0, 0, 100, 100, 0xFF0000});
+	return (0);
 }
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	t_data	data;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Hello world!");
-	img.img = mlx_new_image(mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	my_mlx_pixel_put(&img, 30, 30, 0x0000FF00);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
+			"so_long");
+	if (!data.win_ptr)
+	{
+		free(data.win_ptr);
+		return (MLX_ERROR);
+	}
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+	mlx_loop(data.mlx_ptr);
+	mlx_destroy_window(data.mlx_ptr, data.win_ptr);
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
-
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: angomes- <angomes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 19:16:03 by angomes-          #+#    #+#             */
-/*   Updated: 2023/08/02 19:17:54 by angomes-         ###   ########.fr       */
+/*   Updated: 2023/08/03 16:14:48 by angomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 # define SO_LONG_H
 # define WINDOW_WIDTH 800
 # define WINDOW_HEIGHT 600
+# define MLX_ERROR 1
 
 # include <mlx.h>
-
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <stdlib.h>
 
 #endif
