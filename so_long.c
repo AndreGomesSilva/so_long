a2ecb46cb5718cda962a1ea8a86d81f29bbbbae9
Andre Gomes da silva <angomes-@c3r3p3.42sp.org.br>
2023-08-03 19:52:10 -0300

    struct img

--- so_long.c
+++ so_long.c
@@ -1,10 +1,21 @@
 #include "so_long.h"
 #include <mlx.h>
 
+typedef struct s_img
+{
+   void *mlx_img;
+   char *addr;
+    int bpp;
+    int line_len;
+    int endian;
+} t_img;
+
+
 typedef struct s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+  t_img img;
 }			t_data;
 
 typedef struct s_rect
