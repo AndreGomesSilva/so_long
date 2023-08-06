c033fe856073ef97d0747e8651e2de5227a7a70c
angomes <silva_andregomes@outlook.com>
2023-08-06 18:06:22 -0300

    add mlx mac and linux

---
+++ include/so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angomes- <angomes-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 19:16:03 by angomes-          #+#    #+#             */
+/*   Updated: 2023/08/04 09:24:35 by angomes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# define WINDOW_WIDTH 800
+# define WINDOW_HEIGHT 600
+# define MLX_ERROR 1
+
+# include <mlx.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+#endif
